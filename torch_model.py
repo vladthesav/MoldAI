d12d0f32d9939c06a8d341b95696bf4ee1cea388
vladthesav <47338837+vladthesav@users.noreply.github.com>
2019-09-15 15:05:53 -0700

    Update torch_model.py

--- torch_model.py
+++ torch_model.py
@@ -1,37 +1,42 @@
-import torch
+probabilitieseimport torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import transforms
 
 from efficientnet_pytorch import EfficientNet
 
-
+#define model here
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        #this is the pretrained EfficientNet model which we use as a feature extractor
         self.base = EfficientNet.from_pretrained('efficientnet-b0')
         
+        #fully connected layer which maps features to 7-dim vector space (7 dims for 7 classes)
         self.fc = nn.Linear(1280*7*7, 7)
-
+    
     def forward(self, x):
         x = self.base.extract_features(x)
         x=x.view(x.size(0), -1)
         x = self.fc(x)
         return x
-      
+    #here we apply a softmax activation to get our 'probabilities'  
     def probabilities(self, x):
       linear = self.forward(x)
       return torch.exp(linear)/torch.sum(torch.exp(linear))
-
+    
+#transforms for making Pil images into the data type PyTorch wants
 tfms = transforms.Compose([
     transforms.Resize((224,224)), 
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
+#now let's load our weights into the model
 net = Net()
 net.load_state_dict(torch.load('models/e_net'), strict=False)
 
+#here are all the classes this model was trained to detect
 class_list = ['Chaetomium',
  'Acremonium',
  'Cladosporium',
@@ -40,9 +45,11 @@ class_list = ['Chaetomium',
  'Alternaria',
  'Aureobasidium']
 
+#gets rescaled predictions as a numpy array
 def get_predictions(x):
     return net.probabilities(tfms(x).unsqueeze(0)).detach().numpy()
 
+#now return a dictioany of the form {C_i: P(C_i| mold there and model params)}
 def get_output_dict(x):
     preds = get_predictions(x)[0]
     return {c:d for c,d in zip(class_list,preds)}
