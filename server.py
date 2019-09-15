c59226855580371de22f50862fdde76bf5fbb266
vladthesav <47338837+vladthesav@users.noreply.github.com>
2019-09-15 05:20:27 -0700

    Add files via upload

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request,render_template, redirect
 from torch_model import *
 from PIL import Image
 import numpy as np
+import json
 import cv2
 import io
 import os
@@ -10,22 +11,28 @@ import os
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route('/')
-def home():
-    return render_template('home.html')
+with open('strain_info.json') as json_file:
+    strain_info = json.load(json_file)
 
-@app.route('/index',methods=["GET", "POST"])
+@app.route('/',methods=["GET", "POST"])
 def index():
     yeet = {}
     if request.method == "POST":
         if request.files:
             imagefile = request.files.get('image', '') 
             image = Image.open(imagefile)
-            yeet = get_output_dict(image)
-            print(yeet) 
+            out = get_output_dict(image)
 
-            return render_template("index.html", predictions=yeet)
-    return render_template("index.html", predictions=yeet)
+            stuff = []
+            for k in out.keys():
+                thing = [k, str(100*float(out[k]))[:4]+'%',strain_info[k]['wiki'],strain_info[k]['info']]
+                #stuff.append('<div><h3>'+k + ': ' + str(100*float(out[k]))[:4] + '%<a></h3><p><a href ={}>{}<a></p></div>'.format(strain_info[k]['wiki'],strain_info[k]['info']))
+                stuff.append(thing)
+            
+
+
+            return render_template("index.html", info = stuff)
+    return render_template("index.html", info = stuff)
     
 
 if __name__ == '__main__':
